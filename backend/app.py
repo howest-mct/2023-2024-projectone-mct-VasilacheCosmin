3aea9e778b42fd1c75b17116537761cc2057d67f
CosminVasilache <cosmin.vasilache@student.howest.be>
2024-06-17 01:40:54 +0200

    feat: recup na verlies door merge

--- backend/app.py
+++ backend/app.py
@@ -33,6 +33,8 @@ prev_time = None
 ip_display_last_update = 0
 ip_display_state = 0
 ip_addresses = []
+session_active = False
+duration = 0
 
 GPIO.setmode(GPIO.BCM)
 shutdown_pin = 5
@@ -150,22 +152,52 @@ def get_ip_addresses():
             ip_addresses.append(ip_address)
     return ip_addresses
 
-def display_ip():
+def display_info():
+    global duration
     while True:
-        ip_addresses = get_ip_addresses()
-        if len(ip_addresses) > 2:
-            ip_address = ip_addresses[2]
+        if session_active:
+            hours, remainder = divmod(duration, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            duration_str = f"{hours:02}:{minutes:02}:{seconds:02}"
+            lcd.write_message("Duration:", 1)
+            lcd.write_message(duration_str, 2)
         else:
-            ip_address = "No WLAN IP"
-        if len(ip_address) > 16:
-            ip_address = ip_address[:16]  # Zorg ervoor dat het IP-adres niet langer is dan 16 tekens
-        lcd.write_message("IP wlan0:", 1)
-        lcd.write_message(ip_address, 2)
-        time.sleep(10)  # Update every 10 seconds
+            ip_addresses = get_ip_addresses()
+            if len(ip_addresses) > 2:
+                ip_address = ip_addresses[2]
+            else:
+                ip_address = "No WLAN IP"
+            if len(ip_address) > 16:
+                ip_address = ip_address[:16]
+            lcd.write_message("IP wlan0:", 1)
+            lcd.write_message(ip_address, 2)
+        time.sleep(1)  # Update every second
+
+def start_timer():
+    global session_active, duration
+    session_active = True
+    duration = 0  # Reset duration at the start
+    start_time = time.time()
+    while session_active:
+        duration = int(time.time() - start_time)
+        time.sleep(1)
+
+def stop_timer():
+    global session_active
+    session_active = False
+
+def start_ride():
+    # Start the timer in a new thread to keep track of the duration
+    import threading
+    timer_thread = threading.Thread(target=start_timer)
+    timer_thread.start()
+
+def stop_ride():
+    stop_timer()
 
 # Functie om MPU6050 gegevens op te slaan in de database
-#def save_mpu_data_to_db(timestamp, accel_x, accel_y, accel_z,ritid):
-#    DataRepository.save_mpu_data(timestamp, accel_x, accel_y, accel_z,ritid)
+def save_mpu_data_to_db(timestamp, accel_total,ritid):
+    DataRepository.save_mpu_data(timestamp, accel_total,ritid)
 
 # Functie om LDR gegevens op te slaan in de database
 def save_ldr_data_to_db(timestamp, ldr_value, ritid):
@@ -312,7 +344,7 @@ def initial_connection(auth):
 
 @socketio.on('start_measurement')
 def handle_start_measurement():
-    global reading_thread, reading_thread_stop_event, mpu, mcp
+    global reading_thread, reading_thread_stop_event, mpu, mcp, session_active, duration
     if reading_thread is None or not reading_thread.is_alive():
         start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         save_session(start_time)
@@ -330,10 +362,15 @@ def handle_start_measurement():
         print('Measurement started')
         print(start_time)
         socketio.emit('measurement_started', {'status': 'started'})
+
+        # Start de timer voor de sessieduur
+        session_active = True
+        duration = 0
+        threading.Thread(target=start_timer, daemon=True).start()
         
 @socketio.on('stop_measurement')
 def handle_stop_measurement():
-    global reading_thread_stop_event
+    global reading_thread_stop_event, session_active
     if reading_thread and reading_thread.is_alive():
         reading_thread_stop_event.set()
         reading_thread.join()
@@ -342,6 +379,9 @@ def handle_stop_measurement():
         end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         end_session(end_time)
         display7.clear_display()
+
+        # Stop de timer voor de sessieduur
+        session_active = False
 
 def aan_uit(channel):
     print("test")
@@ -369,7 +409,7 @@ if __name__ == '__main__':
         GPIO.add_event_detect(shutdown_pin, GPIO.FALLING, callback=shutdown, bouncetime=200)
         GPIO.add_event_detect(Aan_Uit_pin, GPIO.FALLING, callback=aan_uit, bouncetime=200)
         initialize_sensors()
-        threading.Thread(target=display_ip, daemon=True).start()  # Start LCD IP display thread
+        threading.Thread(target=display_info, daemon=True).start()  # Start LCD info display thread
         webserver()
     except KeyboardInterrupt:
         print('KeyboardInterrupt exception is caught')
--- front/script/app2.js
+++ front/script/app2.js
@@ -1,288 +1,296 @@
 "use strict";
-
 const lanIP = `${window.location.hostname}:5000`;
 const socketio = io(`http://${lanIP}`);
 
-let coordinates = [];  // Initialize an empty array to store the route coordinates
-let points = [];       // Initialize an empty array to store the points
+let coordinates = []; // Initialize an empty array to store the route coordinates
+let points = []; // Initialize an empty array to store the points
 let totalDistance = 0; // Initialize total distance
-let timerInterval;     // To store the timer interval
-let startTime; 
+let timerInterval; // To store the timer interval
+let startTime;
 
 const shutdown = function () {
-  fetch(`http://${lanIP}/shutdown`, {method: 'POST'})
-  .then(response => {
-    if (response.ok) {
-      alert('Raspberry Pi is shutting down');
-    }
-    else {
-      response.text().then(text => alert(`Shutdown request failed: ${text}`));
-    }
-  })
-  .catch(error => alert(`Shutdown request failed: ${error}`));
+fetch(`http://${lanIP}/shutdown`, {method: 'POST'})
+.then(response => {
+if (response.ok) {
+alert('Raspberry Pi is shutting down');
+}
+else {
+response.text().then(text => alert(`Shutdown request failed: ${text}`));
+}
+})
+.catch(error => alert(`Shutdown request failed: ${error}`));
 }
 
 const initMap = function() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0aHVybWV5ZnJvaWR0IiwiYSI6ImNsdWJ4eGE5dTBhanUyanAzaTB4ZHN2cW0ifQ.NrOlYkV5MHPVQfUxq8NjjQ';
-  const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [3.250204343433751, 50.82413300539262],
-      zoom: 15
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0aHVybWV5ZnJvaWR0IiwiYSI6ImNsdWJ4eGE5dTBhanUyanAzaTB4ZHN2cW0ifQ.NrOlYkV5MHPVQfUxq8NjjQ';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [3.250204343433751, 50.82413300539262],
+zoom: 15
+});
+
+map.on('load', function () {
+map.addSource('route', {
+'type': 'geojson',
+'data': {
+"type": "Feature",
+"geometry": {
+"type": "LineString",
+"coordinates": coordinates
+}
+}
+});
+
+
+  map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'route',
+      'paint': {
+          'line-color': '#888',
+          'line-width': 5
+      }
   });
 
-  map.on('load', function () {
-      map.addSource('route', {
-          'type': 'geojson',
-          'data': {
-              "type": "Feature",
-              "geometry": {
-                  "type": "LineString",
-                  "coordinates": coordinates
-              }
-          }
-      });
-
-      map.addLayer({
-          'id': 'route',
-          'type': 'line',
-          'source': 'route',
-          'paint': {
-              'line-color': '#888',
-              'line-width': 5
-          }
-      });
-
-      map.addSource('points', {
-          'type': 'geojson',
-          'data': {
-              "type": "FeatureCollection",
-              "features": points
-          }
-      });
-
-      map.addLayer({
-          'id': 'points',
-          'type': 'circle',
-          'source': 'points',
-          'paint': {
-              'circle-radius': 6,
-              'circle-color': '#B42222'
-          }
-      });
-
-      console.log('Map loaded and route source added');
+  map.addSource('points', {
+      'type': 'geojson',
+      'data': {
+          "type": "FeatureCollection",
+          "features": points
+      }
   });
 
-  return map;
+  map.addLayer({
+      'id': 'points',
+      'type': 'circle',
+      'source': 'points',
+      'paint': {
+          'circle-radius': 6,
+          'circle-color': '#B42222'
+      }
+  });
+
+  console.log('Map loaded and route source added');
+});
+
+return map;
 };
 
 const startTimer = function() {
-    startTime = new Date();
+startTime = new Date();
 
-    timerInterval = setInterval(function() {
-        let now = new Date();
-        let elapsedTime = new Date(now - startTime);
-        let hours = String(elapsedTime.getUTCHours()).padStart(2, '0');
-        let minutes = String(elapsedTime.getUTCMinutes()).padStart(2, '0');
-        let seconds = String(elapsedTime.getUTCSeconds()).padStart(2, '0');
-        document.getElementById('timer').textContent = `${hours}:${minutes}:${seconds}`;
-    }, 1000);
+
+timerInterval = setInterval(function() {
+    let now = new Date();
+    let elapsedTime = new Date(now - startTime);
+    let hours = String(elapsedTime.getUTCHours()).padStart(2, '0');
+    let minutes = String(elapsedTime.getUTCMinutes()).padStart(2, '0');
+    let seconds = String(elapsedTime.getUTCSeconds()).padStart(2, '0');
+    document.getElementById('timer').textContent = `${hours}:${minutes}:${seconds}`;
+}, 1000);
 };
 
 const stopTimer = function() {
-    clearInterval(timerInterval);
-    document.getElementById('timer').textContent = "00:00:00"; // Reset timer display to 0
+clearInterval(timerInterval);
+document.getElementById('timer').textContent = "00:00:00"; // Reset timer display to 0
 };
-
 
 let map;
 
 const startMeasuring = function() {
-  socketio.emit('start_measurement');
-  startTimer();
+socketio.emit('start_measurement');
+startTimer();
 };
 
 const stopMeasuring = function() {
-  socketio.emit('stop_measurement');
-  stopTimer();
-    totalDistance = 0; // Reset total distance
-    document.getElementById('distance').textContent = totalDistance.toFixed(2); // Reset distance display to 0
-    coordinates = []; // Clear the coordinates array
-    points = []; // Clear the points array
+socketio.emit('stop_measurement');
+stopTimer();
+totalDistance = 0; // Reset total distance
+document.getElementById('distance').textContent = totalDistance.toFixed(2); // Reset distance display to 0
+coordinates = []; // Clear the coordinates array
+points = []; // Clear the points array
 };
 
 const listenToUI = function () {
-  const startButton = document.getElementById('startButton');
-  const stopButton = document.getElementById('stopButton');
-  const ldrContainer = document.getElementById('ldrContainer');
-  const mpuContainer = document.getElementById('mpuContainer');
-  const gpsContainer = document.getElementById('gpsContainer');
-  const searchButton = document.getElementById('searchButton');
-  const mapContainer = document.getElementById('mapContainer');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const ldrContainer = document.getElementById('ldrContainer');
+const mpuContainer = document.getElementById('mpuContainer');
+const gpsContainer = document.getElementById('gpsContainer');
+const searchButton = document.getElementById('searchButton');
+const mapContainer = document.getElementById('mapContainer');
 
-  if (startButton && stopButton && ldrContainer && mpuContainer && gpsContainer) {
-    startButton.addEventListener('click', function() {
-      console.log('Startknop ingedrukt');
-      startMeasuring();
-      ldrContainer.classList.remove('hidden');
-      mpuContainer.classList.remove('hidden');
-      gpsContainer.classList.remove('hidden');
-      mapContainer.classList.remove('hidden');
-      totalDistance = 0; // Reset distance at the start of measurement
-      document.getElementById('distance').textContent = totalDistance; // Reset distance display
-    });
+if (startButton && stopButton && ldrContainer && mpuContainer && gpsContainer) {
+startButton.addEventListener('click', function() {
+console.log('Startknop ingedrukt');
+startMeasuring();
+ldrContainer.classList.remove('hidden');
+mpuContainer.classList.remove('hidden');
+gpsContainer.classList.remove('hidden');
+mapContainer.classList.remove('hidden');
+startButton.classList.add('hidden');
+stopButton.classList.remove('hidden');
+totalDistance = 0; // Reset distance at the start of measurement
+document.getElementById('distance').textContent = totalDistance; // Reset distance display
+});
 
-    stopButton.addEventListener('click', function() {
-      console.log('Stopknop ingedrukt');
-      stopMeasuring();
-      ldrContainer.classList.add('hidden');
-      mpuContainer.classList.add('hidden');
-      gpsContainer.classList.add('hidden');
-      mapContainer.classList.remove('hidden');
-    });
-  }
 
-  if (searchButton) {
-    searchButton.addEventListener('click', function() {
-      const ritId = document.getElementById('ritIdInput').value;
-      if (ritId) {
-        initCharts(ritId);
-      }
-    });
-  }
+stopButton.addEventListener('click', function() {
+  console.log('Stopknop ingedrukt');
+  stopMeasuring();
+  ldrContainer.classList.add('hidden');
+  mpuContainer.classList.add('hidden');
+  gpsContainer.classList.add('hidden');
+  mapContainer.classList.add('hidden');
+  startButton.classList.remove('hidden');
+  stopButton.classList.add('hidden');
+});
+}
+
+if (searchButton) {
+searchButton.addEventListener('click', function() {
+const ritId = document.getElementById('ritIdInput').value;
+if (ritId) {
+initCharts(ritId);
+}
+});
+}
 };
 
 const calculateDistance = function(coord1, coord2) {
-  const toRad = function(value) {
-    return value * Math.PI / 180;
-  };
+const toRad = function(value) {
+return value * Math.PI / 180;
+};
 
-  const lat1 = coord1[1];
-  const lon1 = coord1[0];
-  const lat2 = coord2[1];
-  const lon2 = coord2[0];
+const lat1 = coord1[1];
+const lon1 = coord1[0];
+const lat2 = coord2[1];
+const lon2 = coord2[0];
 
-  const R = 6371; // Radius of the Earth in km
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const d = R * c * 1000; // Distance in meters
+const R = 6371; // Radius of the Earth in km
+const dLat = toRad(lat2 - lat1);
+const dLon = toRad(lon2 - lon1);
+const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+Math.sin(dLon / 2) * Math.sin(dLon / 2);
+const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+const d = R * c * 1000; // Distance in meters
+console.log(d);
 
-  return d;
+return d;
 };
 
 const listenToSocket = function () {
-  socketio.on('connect', function () {
-    console.log('Verbonden met socket webserver');
-  });
+socketio.on('connect', function () {
+console.log('Verbonden met socket webserver');
+});
 
-  socketio.on('B2F_LDR_DATA', function(data) {
-    console.log('Data LDR ontvangen', data);
-    const ldrElement = document.getElementById('ldr');
-    if (ldrElement) {
-      ldrElement.innerHTML = data.ldr_value.toFixed(0);
+socketio.on('B2F_LDR_DATA', function(data) {
+console.log('Data LDR ontvangen', data);
+const ldrElement = document.getElementById('ldr');
+if (ldrElement) {
+ldrElement.innerHTML = data.ldr_value.toFixed(0);
+}
+});
+
+socketio.on('B2F_MPU6050_DATA', function(data) {
+console.log('Data MPU ontvangen', data);
+const timestampElement = document.getElementById('mpu6050-timestamp');
+const accelTotalElement = document.getElementById('mpu6050-accel-total');
+
+
+if (timestampElement && accelTotalElement ) {
+  timestampElement.innerHTML = 'Timestamp: ' + data.timestamp;
+  accelTotalElement.innerHTML = data.total_accel.toFixed(2);
+}
+});
+
+socketio.on('B2F_GPS_DATA', function(data) {
+console.log('GPS data ontvangen:', data);
+const latElement = document.getElementById('gps-Lat');
+const lonElement = document.getElementById('gps-long');
+const speedElement = document.getElementById('gps-speed');
+// console.log('Dit zijn de elementen speed - long - lat ')
+// console.log(speedElement)
+// console.log(lonElement)
+// console.log(latElement)
+
+
+if (latElement && lonElement && speedElement) {
+  latElement.innerHTML = data.lat.toFixed(6);
+  lonElement.innerHTML = data.lon.toFixed(6);
+  speedElement.innerHTML = data.speed.toFixed(2);
+}
+
+// Update the route and points on the map
+const newCoord = [data.lon, data.lat];
+coordinates.push(newCoord);
+
+if (coordinates.length > 1) {
+  const lastCoord = coordinates[coordinates.length - 2];
+  totalDistance += calculateDistance(lastCoord, newCoord);
+  console.log(totalDistance);
+  document.getElementById('distance').textContent = totalDistance.toFixed(2); // Update distance display
+}
+
+points.push({
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": newCoord
+    }
+});
+
+if (map.getSource('route')) {
+  console.log('Updating route on map with coordinates', coordinates);
+  map.getSource('route').setData({
+    "type": "Feature",
+    "geometry": {
+      "type": "LineString",
+      "coordinates": coordinates
     }
   });
+} else {
+  console.log('Route source not found on map');
+}
 
-  socketio.on('B2F_MPU6050_DATA', function(data) {
-    console.log('Data MPU ontvangen', data);
-    const timestampElement = document.getElementById('mpu6050-timestamp');
-    const accelTotalElement = document.getElementById('mpu6050-accel-total');
-
-    if (timestampElement && accelTotalElement ) {
-      timestampElement.innerHTML = 'Timestamp: ' + data.timestamp;
-      accelTotalElement.innerHTML = data.total_accel.toFixed(2);
-    }
+if (map.getSource('points')) {
+  console.log('Updating points on map with coordinates', points);
+  map.getSource('points').setData({
+    "type": "FeatureCollection",
+    "features": points
   });
+} else {
+  console.log('Points source not found on map');
+}
 
-  socketio.on('B2F_GPS_DATA', function(data) {
-    console.log('GPS data ontvangen:', data);
-    const latElement = document.getElementById('gps-Lat');
-    const lonElement = document.getElementById('gps-long');
-    const speedElement = document.getElementById('gps-speed');
-    // console.log('Dit zijn de elementen speed - long - lat ')
-    // console.log(speedElement)
-    // console.log(lonElement)
-    // console.log(latElement)
+// Center the map on the new location
+map.setCenter(newCoord);
+});
 
-    if (latElement && lonElement && speedElement) {
-      latElement.innerHTML = data.lat.toFixed(6);
-      lonElement.innerHTML = data.lon.toFixed(6);
-      speedElement.innerHTML = data.speed.toFixed(2);
-    }
+socketio.on('measurement_started', function(data) {
+console.log('Measurement started', data);
+});
 
-    // Update the route and points on the map
-    const newCoord = [data.lon, data.lat];
-    coordinates.push(newCoord);
+socketio.on('measurement_stopped', function(data) {
+console.log('Measurement stopped', data);
+const ldrElement = document.getElementById('ldr');
+if (ldrElement) {
+ldrElement.innerHTML = "N/A";
+}
+});
 
-    if (coordinates.length > 1) {
-      const lastCoord = coordinates[coordinates.length - 2];
-      totalDistance += calculateDistance(lastCoord, newCoord);
-      document.getElementById('distance').textContent = totalDistance.toFixed(2); // Update distance display
-    }
-
-    points.push({
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": newCoord
-        }
-    });
-
-    if (map.getSource('route')) {
-      console.log('Updating route on map with coordinates', coordinates);
-      map.getSource('route').setData({
-        "type": "Feature",
-        "geometry": {
-          "type": "LineString",
-          "coordinates": coordinates
-        }
-      });
-    } else {
-      console.log('Route source not found on map');
-    }
-
-    if (map.getSource('points')) {
-      console.log('Updating points on map with coordinates', points);
-      map.getSource('points').setData({
-        "type": "FeatureCollection",
-        "features": points
-      });
-    } else {
-      console.log('Points source not found on map');
-    }
-
-    // Center the map on the new location
-    map.setCenter(newCoord);
-  });
-  
-
-  socketio.on('measurement_started', function(data) {
-    console.log('Measurement started', data);
-  });
-
-  socketio.on('measurement_stopped', function(data) {
-    console.log('Measurement stopped', data);
-    const ldrElement = document.getElementById('ldr');
-    if (ldrElement) {
-      ldrElement.innerHTML = "N/A";
-    }
-  });
-
-  socketio.on('disconnect', function () {
-    console.log('Verbinding met socket webserver verbroken');
-  });
+socketio.on('disconnect', function () {
+console.log('Verbinding met socket webserver verbroken');
+});
 };
 
 const init = function () {
-  console.info('DOM geladen');
-  listenToUI();
-  listenToSocket();
-  map = initMap(); // Initialize the map
+console.info('DOM geladen');
+listenToUI();
+listenToSocket();
+map = initMap(); // Initialize the map
 };
 
 document.addEventListener('DOMContentLoaded', init);
